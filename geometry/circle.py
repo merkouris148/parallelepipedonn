02e57fec57af93391b66de8589dc1a22bd530e0f
Merkouris Papamichail <33814134+merkouris148@users.noreply.github.com>
2026-05-13 15:15:20 +0300

    compatability with numpy

--- geometry/circle.py
+++ geometry/circle.py
@@ -13,6 +13,9 @@
 # 3rd party libraries
 import numpy as np
 
+# typing
+from typing import Callable
+
 # Custom libraries
 import geometry.interval as interval
 import geometry.norms as norms
@@ -34,11 +37,16 @@ from geometry.constants import epsilon
 # each subclass
 ###########################################################
 class Circle:
+    """
+        * The base class for circles.
+        * Each subclass should be defined by giving an alternate norm.
+        * The "in" operator is implemented once and works for each subclass
+    """
     def __init__(
             self, 
-            center,     # center of the circle
-            radius,     # radius of the circle
-            norm        # a function IR^d x IR^d --> IR_{>= 0}
+            center: np.ndarray,                                 # center of the circle
+            radius: float,                                      # radius of the circle
+            norm:   Callable[[np.ndarray, np.ndarray], float]   # a function IR^d x IR^d --> IR_{>= 0}
         ):
         assert radius >= 0
 
@@ -57,26 +65,42 @@ class Circle:
 
 
     ## Accessors
-    def get_radius(self):
+    def get_radius(self) -> float:
+        """
+            Return the radius
+        """
         return self.radius
     
-    def get_center(self):
+    def get_center(self) -> np.ndarray:
+        """
+            Return the radius
+        """
         return self.radius
     
     ## Mutators
-    def set_radius(self, new_radius):
+    def set_radius(self, new_radius:float):
+        """
+            Set radius
+        """
         assert new_radius >= 0
 
         self.radius = new_radius
     
-    def set_center(self, new_center):
+    def set_center(self, new_center:np.ndarray):
+        """
+            Set the center
+        """
         assert new_center.shape[0] == self.row_dim
         assert new_center.shape[1] == self.column_dim
 
         self.center = new_center
     
     ## Predicates
-    def __contains__(self, x):
+    def __contains__(self, x:np.ndarray):
+        """
+            Check if x \in C, i.e.,
+            ``||x - c ||_+oo <= r``.
+        """
         assert x.shape[0] == self.row_dim
         assert x.shape[1] == self.column_dim
 
@@ -105,6 +129,10 @@ class Circle:
 # conversion method
 ###########################################################
 class InfCircle(Circle):
+    """
+        * The infinity-norm circle.
+        * Since inf-circle is a box in IR^d, we implement a conversion method
+    """
     ## Initialization
     def __init__(self, center, radius):
 
@@ -112,7 +140,11 @@ class InfCircle(Circle):
         super().__init__(center, radius, norms.inf_norm)
 
     ## Conversions
-    def get_interval(self):
+    def get_interval(self) -> interval.Interval:
+        """
+            Convert the l_+oo circle to interval. Note that
+            l_+oo circles correspond to hyper-cubes.
+        """
         return interval.Interval(
             self.center - self.radius * np.ones((self.row_dim, self.column_dim)),
             self.center + self.radius * np.ones((self.row_dim, self.column_dim))
--- geometry/interval.py
+++ geometry/interval.py
@@ -3,17 +3,39 @@
 # --------------------------------------------------------
 # A library implementing an interval in IR^d
 ###########################################################
+#############
+# Constants #
+#############
+warnings = True
 
 
 #############
 # Libraries #
 #############
+import typing as t
+import itertools
+
 # 3rd party libraries
+#from pprint import pformat
 import numpy as np
 
-# custom libraries
-from geometry.norms import inf_norm
-from geometry.constants import epsilon
+
+# arbitrary percision arithmetic
+import sys
+mpmath_lib_name = "mpmath"
+try:
+    import mpmath as mpm # type: ignore
+except ImportError as _:
+    if warnings:
+        print("Warning [geometry.intervals]: mpmath is not installed!")
+        print("Arbitrary Percision Arithmetic is disabled")
+
+
+#custom libraries
+# from norms import inf_norm
+# from constants import epsilon
+# from norms import inf_norm
+# from constants import epsilon
 
 
 ###########################################################
@@ -25,128 +47,359 @@ from geometry.constants import epsilon
 # in each subclass.
 ###########################################################
 class Interval:
+    """
+        A class encoding a *high-dimensional* interval `[lb, ub]`, where\\
+        `lb, ub \in IR^d`. An interval is defined as:
+        ```
+        [lb, ub] = {x \in IR^d | lb <= x <= ub}
+        ```
+        **Data Members:**
+        * `lb: np.ndarray`, the lower bound.
+        * `ub: np.ndarray`, the upper bound.
 
-    ## Constructor
-    def __init__(self, lb, ub):
+        **Notes:**
+        * The class is completely compatible with NumPy's ndarrays and\\
+        is able to handle general multidimensional arrays instead of\\
+        1D vectors.
+
+        **Refferences:**
+        * Theory of an Interval Algebra and Its Application to Numerical Analysis --
+        Teruo Sunaga (1958).
+    """
+    ################
+    # Constructors #
+    ################
+    def __init__(self, lb: np.ndarray, ub: np.ndarray):
+        """
+            The default constructor.
+        """
         assert lb.shape == ub.shape
-        assert (lb <= ub).all()
 
         ## Get dimenstions
-        self.row_dim    = lb.shape[0]
-        self.column_dim = lb.shape[1]
-        self.dim        = self.row_dim * self.column_dim
+        self.shape  = lb.shape
+        self.dim    = np.prod(np.array(self.shape))
 
         ## Store lb, ub
         self.lb = lb
         self.ub = ub
     
     def __copy__(self):
+        """
+            Copy constructor.
+        """
         return Interval(self.lb.copy(), self.ub.copy())
     
-    ## Local Invariant Concistency
-    def inequality_consistency(self, ind):
-        return self.ub[ind] >= self.lb[ind]
 
-    ## Global Invariant Concistency 
-    def inequalities_consistency(self):
-        return (self.ub >= self.lb).all()
+    ##########
+    # Report #
+    ##########
+    def __str__(self) -> str:
+        """
+            Short `str` report.
+        """
+        s = "interval[\n"
+        s +=  str(self.lb) + ",\n"
+        s +=  str(self.ub) + "\n"
+        s += "]"
+
+        return s
+
+    def extended_str(self) -> str:
+        """
+            Extentent `str` report.
+        """
+        s = "\n\n" + str(self) + "\n\n"
+
+        s += f"{'Diam.:':<17}"          + str(self.diam())              + "\n"
+        s += f"{'Min. Edge Len.:':<17}" + str(self.min_edge_length())   + "\n"
+        s += f"{'Avg. Edge Len.:':<17}" + str(self.avg_edge_len())      + "\n"
+        s += f"{'Vol.:':<17}"            + str(self.vol())               + "\n"
+
+        return s
     
+    def report(self):
+        """
+            Print report
+        """
+        print(self.extended_str())
+
+    ##############
+    # Predicates #
+    ##############
+    # Bellow we use a hacky way to force the returned type to be bool.
+    # Note that NumPy returns a np.bool_ type, with np.bool_ != bool.
+    # This is an inherited problem from NumPy. See:
+    # https://github.com/python/mypy/issues/10385
+    def empty(self) -> bool:
+        """
+            Checks if the interval is empty, i.e.,
+            `ub < lb`.
+        """
+        return ((self.ub < self.lb).all()) == True
     
-    ## Accessors
-    def get_interval(self):
-        return self
+    def sigleton(self) -> bool:
+        """
+            Check if the interval is a sigleton, i.e.,
+            `[x, x]`.
+        """
+        return ((self.ub == self.lb).all()) == True
 
-
-    ## Mutators
-    def update_lb(self, ind, val):
-        assert 0 <= ind[0] and ind[0] <= self.row_dim
-        assert 0 <= ind[1] and ind[1] <= self.column_dim
-
-        self.lb[ind] = val
-
-        assert self.inequality_consistency(ind)
+    def is_positive(self) -> bool:
+        """
+            Check if the interval belongs to the positive
+            quadrant.
+        """
+        O = np.zeros(self.shape)
+        return ((self.ub >= O).all() and (self.lb >= O).all()) == True
     
-    def update_ub(self, ind, val):
-        assert 0 <= ind[0] and ind[0] <= self.row_dim
-        assert 0 <= ind[1] and ind[1] <= self.column_dim
-
-        self.ub[ind] = val
-
-        assert self.inequality_consistency(ind)
+    def is_negative(self) -> bool:
+        """
+            Check if the interval belongs to the negative
+            quadrant.
+        """
+        O = np.zeros(self.shape)
+        return ((self.ub <= O).all() and (self.lb <= O).all()) == True
     
+    ####################
+    # Interval Algebra #
+    ####################
+    ## in operator
+    def __contains__(self, x) -> bool:
+        """
+            Checking if `x \in I`.
+        """
+        if self.shape != x.shape: return False
+        return (self.lb <= x).all() and (x <= self.ub).all()
 
-    ## Interval Algebra
-    def __contains__(self, x):
-        assert x.shape[0] == self.row_dim
-        assert x.shape[1] == self.column_dim
-
-        return (x - self.lb >= -epsilon).all() and (self.ub - x >= -epsilon).all()
-
+    ## Equality & Inequality
     def __eq__(self, interval) -> bool:
-        if interval.row_dim     != self.row_dim:       return False
-        if interval.column_dim  != self.column_dim:    return False
+        """
+            Checking if `I1 == I2`. Note that two empty intervals
+            are *always* equal.
+        """
+        ## Adhering to Liskov substitution principle
+        # Something that is not interval cannot be equal to an
+        # interval
+        if not isinstance(interval, Interval): return False
 
+        if self.empty() and interval.empty(): return True
+
+        if self.shape != interval.shape: return False
         return (self.lb == interval.lb).all() and (self.ub == interval.ub).all()
 
-    def includes(self, interv):
-        return interv.lb in self and interv.ub in self
+    def __ne__(self, interval) -> bool:
+        """
+            Checking if `I1 != I2`.
+        """
+        return not self == interval
 
-    ## self <-- self \cap interval
+    ## Comparisons
+    def __lt__(self, interval) -> bool:
+        """
+            Checking if `I1 \subset I2`.
+        """
+        return (interval.lb < self.lb).all() and (self.ub < interval.ub).all()
+
+    def __le__(self, interval) -> bool:
+        """
+            Checking if `I1 \subseteq I2`.
+        """
+        return (interval.lb <= self.lb).all() and (self.ub <= interval.ub).all()
+
+    def __gt__(self, interval) -> bool:
+        """
+            Checking if `I1 \supset I2`.
+        """
+        return (interval.lb > self.lb).all() and (self.ub > interval.ub).all()
+
+    def __ge__(self, interval) -> bool:
+        """
+            Checking if `I1 \supseteq I2`.
+        """
+        return (interval.lb >= self.lb).all() and (self.ub >= interval.ub).all()
+
+    ## Operations on Intervals
+    ## self <-- self \sqcup interval
+    def join(self, interval):
+        """
+            Join operation on the interval latice. In math,
+            ```
+            I_1 <-- I_1 \/ I_2
+            ```
+        """
+        assert self.shape == interval.shape
+
+        self.lb = np.minimum(self.lb, interval.lb)
+        self.ub = np.maximum(self.ub, interval.ub)
+    
+    def __or__(self, interval):
+        """
+            Join operation on the interval latice. In math,
+            ```
+            I = I_1 \/ I_2
+            ```
+            In python, we write:
+            ```
+            I = I_1 | I_2
+            ```
+        """
+        assert self.shape == interval.shape
+
+        lb_new = np.minimum(self.lb, interval.lb)
+        ub_new = np.maximum(self.ub, interval.ub)
+
+        return Interval(lb_new.copy(), ub_new.copy())
+
+
+    # self <-- self \cap interval
     def intersect(self, interval):
-        ## Preconditions
-        assert self.row_dim     == interval.row_dim
-        assert self.column_dim  == interval.column_dim
-        assert (interval.ub >= interval.lb).all()
+        return self.meet(interval)
+
+    def meet(self, interval):
+        """
+            Meet operation on the interval latice. In math,
+            ```
+            I_1 <-- I_1 /\ I_2
+            ```
+        """
+        assert self.shape == interval.shape
 
         self.lb = np.maximum(self.lb, interval.lb)
         self.ub = np.minimum(self.ub, interval.ub)
 
-        ## Postcondition
-        assert self.inequalities_consistency()
-    
-    ## self <-- self \sqcup interval
-    def concatenate(self, interval):
-        ## Preconditions
-        assert self.row_dim     == interval.row_dim
-        assert self.column_dim  == interval.column_dim
-        assert (interval.ub >= interval.lb).all()
+    def __and__(self, interval):
+        """
+            Meet operation on the interval latice. In math,
+            ```
+            I = I_1 /\ I_2
+            ```
+            In python, we write:
+            ```
+            I = I_1 & I_2
+            ```
+        """
+        assert self.shape == interval.shape
 
-        self.lb = np.minimum(self.lb, interval.lb)
-        self.ub = np.maximum(self.ub, interval.ub)
+        lb_new = np.maximum(self.lb, interval.lb)
+        ub_new = np.minimum(self.ub, interval.ub)
 
-        ## Postcondition
-        assert self.inequalities_consistency()
+        return Interval(lb_new.copy(), ub_new.copy())
 
 
     # Minkowski sum
     def __add__(self, interval):
+        """
+            Adding 2 intervals, i.e., `I = I_1 + I_2`.
+        """
+        assert self.shape == interval.shape
+
         lb_new = self.lb + interval.lb
         ub_new = self.ub + interval.ub
 
-        new_interval = Interval(lb_new, ub_new)
-        return new_interval
-        
+        return Interval(lb_new.copy(), ub_new.copy())
+    
+
     def __sub__(self, interval):
+        """
+            Subtractig 2 intervals, i.e., `I = I_1 - I_2`.
+        """
+        assert self.shape == interval.shape
+
         lb_new = self.lb - interval.lb
         ub_new = self.ub - interval.ub
 
         new_interval = Interval(lb_new, ub_new)
         return new_interval
 
-    ## Metrics
-    # Average coordinate-wise diameter as potential
-    def calc_potential(self):
-        return round(np.sum(self.ub - self.lb) / self.dim, 4)
 
-    def get_diameter(self):
+    ## Vertices
+    def get_vertices(self) -> np.ndarray:
+        """
+            Return the Interval's Vertices. Only for vector
+            endpoints (ndim == 1) and dim <= 10
+        """
+
+        assert self.lb.ndim == 1,\
+        "Intervals.get_vertices works only for intervals\
+        with vector endpoints, i.e. ndim == 1."
+        assert self.lb.shape[0] <= 10,\
+        "Intervals.get_vertices works only for intervals\
+        with low dim vector endpoints, i.e. dim <= 10."
+
+        Vertices    = []
+        dim         = self.lb.shape[0]
+        for bin_vec in itertools.product([0,1], repeat=dim):
+            vertex = np.zeros((dim,))
+            for ind in range(dim):
+                if bin_vec[ind] == 0:   vertex[ind] = self.lb[ind]
+                else:                   vertex[ind] = self.ub[ind]
+            
+            Vertices.append(vertex)
+        
+        return np.array(Vertices)
+            
+            
+
+    ## Metrics
+    def diam(self):
+        """
+            Computing the *diameter* of the interval, i.e.,
+            ```
+            diam = ||lb - ub||_{+oo}
+            ```
+        """
         return inf_norm(self.ub - self.lb)
 
-    def volume(self):
+
+    def vol(self):
+        """
+            Computing the *volume* of the interval. If `mpmath` is imported,
+            we use arbitrary percision arithmetic. In not, there may be some
+            serious underflow in the volume computation.
+        """
         edge_lengths    = np.abs(self.ub - self.lb)
-        vol             = np.prod(edge_lengths, dtype=np.int64)
+        vol             = 0.0
+
+        if mpmath_lib_name not in sys.modules:
+            if warnings: 
+                print("Warning [geometry.intervals]: mpmath is not installed!")
+                print("Volume computation might be inacurate!")
+            
+            vol = np.prod(edge_lengths, dtype=np.float64)
+        
+        else:
+            with mpm.workdps(self.dim):
+                vol = mpm.fprod(edge_lengths.flatten())
 
         return vol
     
+
+    # Average coordinate-wise diameter as potential
+    def avg_edge_len(self):
+        """
+            Computing the *average* edge length.
+        """
+        return round(np.sum(self.ub - self.lb) / self.dim, 4)
+
+
     def min_edge_length(self):
+        """
+            Computing *minimum* edge length.
+        """
         return np.min(self.ub - self.lb)
+    
+    
+    ############
+    # Sampling #
+    ############
+    def random_points(self, N=1000, seed=0):
+        """
+            Returning `N` number of *uniformly* drawn points inside the
+            interval. With `seed` we denote the seed of the Random Number
+            Generator.
+        """
+        assert N > 0
+        rnjeasus    = np.random.default_rng(seed)
+        out_shape   = tuple([N] + list(self.shape))
+        return rnjeasus.uniform(self.lb, self.ub, out_shape)
     
